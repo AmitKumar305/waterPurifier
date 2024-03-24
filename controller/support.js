58c74e522d45adc4998c3fd526f381fcd7d6ab82
Amit Kumar <k.amitkumar305@gmail.com>
2024-03-24 21:20:42 +0530

    Removed console.log

--- controller/support.js
+++ controller/support.js
@@ -2,7 +2,6 @@ const sendEmail = require('../services/sendEmail');
 
 const support = async (req, res) => {
     const { fullName, phoneNumber, billNumber, subject, issue } = req.body;
-    console.log(fullName);
 
     sendEmail({
         fullName, phoneNumber, billNumber, subject, issue, file: req.files.file.data, fileName: req.files.file.name, type: 'support',
