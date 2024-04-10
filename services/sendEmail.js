33a3358327c6fabdad7241ed4a42bffedbca169d
Amit Kumar <k.amitkumar305@gmail.com>
2024-04-10 19:17:09 +0530

    Removed await.

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -3,57 +3,61 @@ const nodemailer = require('nodemailer');
 const sendContactUsEmail = async ({
     fullName = '', phoneNumber = '', alternatePhoneNumber = '', subject = '', issue = '', billNumber = '', file, fileName, type,
 }) => {
-    const transpoter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: process.env.BUSINESS_EMAIL,
-            pass: process.env.BUSINESS_PASSWORD,
-        },
-        secure: false,
-        port: 25, 
-        tls: {
-            rejectUnauthorized: false
-        },
-    });
-
-    let mailOptions = {};
-    if (type === 'contactUs') {
-        mailOptions = {
-            from: '',
-            to: process.env.BUSINESS_EMAIL,
-            subject: 'Contact Us Form',
-            text: `
-                Name: ${fullName}
-                Phone Number: ${phoneNumber}
-                Alternate Phone Number: ${alternatePhoneNumber}
-                Subject: ${subject}
-                Issue: ${issue}
-            `,
-        };
-    } else {
-        mailOptions = {
-            from: '',
-            to: process.env.BUSINESS_EMAIL,
-            subject: 'Support Form',
-            text: `
-                Name: ${fullName}
-                Phone Number: ${phoneNumber}
-                Bill Number: ${billNumber}
-                Subject: ${subject}
-                Issue: ${issue}
-            `
-        };
-        if (file && fileName) {
-            mailOptions.attachments = [
-                {
-                  filename: fileName,
-                  content: file,
-                },
-            ];
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: process.env.BUSINESS_EMAIL,
+                pass: process.env.BUSINESS_PASSWORD,
+            },
+            secure: false,
+            port: 25, 
+            tls: {
+                rejectUnauthorized: false
+            },
+        });
+    
+        let mailOptions = {};
+        if (type === 'contactUs') {
+            mailOptions = {
+                from: '',
+                to: process.env.BUSINESS_EMAIL,
+                subject: 'Contact Us Form',
+                text: `
+                    Name: ${fullName}
+                    Phone Number: ${phoneNumber}
+                    Alternate Phone Number: ${alternatePhoneNumber}
+                    Subject: ${subject}
+                    Issue: ${issue}
+                `,
+            };
+        } else {
+            mailOptions = {
+                from: '',
+                to: process.env.BUSINESS_EMAIL,
+                subject: 'Support Form',
+                text: `
+                    Name: ${fullName}
+                    Phone Number: ${phoneNumber}
+                    Bill Number: ${billNumber}
+                    Subject: ${subject}
+                    Issue: ${issue}
+                `
+            };
+            if (file && fileName) {
+                mailOptions.attachments = [
+                    {
+                      filename: fileName,
+                      content: file,
+                    },
+                ];
+            }
         }
+        
+        transporter.sendMail(mailOptions);        
+    } catch (err) {
+        console.log(err);
     }
-
-    await transpoter.sendMail(mailOptions);
 };
 
 module.exports = sendContactUsEmail;
